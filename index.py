5af4a5f066c107bff9ba48fc602cbe305a11c811
Umut Can Bayındır <83068943+umtcnbyndr@users.noreply.github.com>
2023-07-11 12:02:27 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,3 @@
-# BankAccount isminde bir sınıf tanımlayınız.
-# Üretilen her bir nesne owner isminde biz özelliğe sahip olmalıdır. BankAccount("Sadık Turan")
-# Üretilen her bir nesne balance isminde bir özelliğe sahip olup başlangıçta 0.0 değerinde olmalıdır.
-# Üretilen her bir nesne için deposit metodu oluşturun ve dışarıdan yatırılacak miktar bilgisini alıp balance
-# üzerine ekleyin ve balance miktarını geriye döndürün.
-# Üretilen her bir nesne için withdraw metodu oluşturun ve dışarıdan çekilecek miktar bilgisini alıp balance
-# değerinden çıkarıp geriye döndürün.
-
-# hesap = BankAccount("Sadık Turan")
-# hesap.owner => Sadık Turan
-# hesap.balance => 0.0
-# hesap.deposit(1000) => 1000.0
-# hesap.withdraw(500) => 500.0
-
-
 class BankAccount:
     def __init__(self,name):
         self.name = name
